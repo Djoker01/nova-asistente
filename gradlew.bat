0d40ea8c9324c786578e24ec15b6bfd9dd4ac34a
Djoker01 <113226157+Djoker01@users.noreply.github.com>
2026-06-14 16:37:00 -0400

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -21,6 +21,9 @@ set JAVA_EXE=java.exe
 if "%ERRORLEVEL%" == "0" goto execute
 
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
 goto fail
 
 :findJavaFromJavaHome
@@ -30,6 +33,9 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 if exist "%JAVA_EXE%" goto execute
 
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
 goto fail
 
 :execute
